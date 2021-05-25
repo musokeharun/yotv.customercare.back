472932d33507d2ca8fcff344408322481cfcdd72
Haruna H <musokeharun@gmail.com>
2021-05-25 18:00:27 +0300

    Update To Update To Better

--- chunk/index.js
+++ chunk/index.js
@@ -1,172 +1,236 @@
-const { PrismaClient } = require("@prisma/client");
+const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
 
 let today = new Date();
 today.setUTCHours(0, 0, 0, 0);
 
 const Chunk = {
-  // DEPRECATED
-  hasChunk: async (email) => {
-    let chunk = await prisma.chunk.findFirst({
-      where: {
-        user: { email },
-        createdAt: {
-          gte: today,
-        },
-      },
-      select: {
-        id: true,
-        createdAt: true,
-      },
-    });
-    if (!chunk || chunk == null) return false;
-    return chunk["id"];
-  },
-
-  getAvailableChunkId: async (email) => {
-    let registeredChunk = await prisma.chunk.findFirst({
-      where: {
-        user: { email },
-        status: true,
-      },
-      select: {
-        id: true,
-        bulk: {
-          select: {
-            id: true,
-            data: true,
-          },
-        },
-      },
-    });
-
-    console.log(registeredChunk);
-
-    if (registeredChunk) {
-      return registeredChunk.id;
-    }
-
-    let bulkId = await Chunk.getAvailabeBulkId();
-
-    let createdChunk = await prisma.chunk.create({
-      data: {
-        user: {
-          connect: {
-            email,
-          },
-        },
-        bulk: {
-          connect: {
-            id: bulkId,
-          },
-        },
-      },
-    });
-
-    return createdChunk.id;
-  },
-
-  getAvailabeBulkId: async () => {
-    let currentBulk = await prisma.bulk.findFirst({
-      where: {
-        status: true,
-      },
-      select: {
-        id: true,
-        data: true,
-        popIndex: true,
-      },
-    });
-
-    let i = 0;
-    while (typeof currentBulk.data !== "object") {
-      currentBulk.data = JSON.parse(currentBulk.data);
-      i++;
-      if (i >= 4) break;
-    }
-
-    if (currentBulk) {
-      const { popIndex, data } = currentBulk;
-      console.log("Bulk", currentBulk.id);
-
-      if (popIndex >= data.length - 1 || popIndex >= data.length) {
-        let updatedOldBulk = await prisma.bulk.update({
-          where: {
-            id: currentBulk.id,
-          },
-          data: {
-            status: false,
-          },
-        });
-
-        let newBulk = await prisma.bulk.findFirst({
-          where: {
-            status: false,
-            id: {
-              gte: currentBulk.id,
-            },
-          },
-          select: {
-            id: true,
-            popIndex: true,
-          },
-        });
-
-        if (newBulk && popIndex == 0) {
-          let newBulk = await prisma.bulk.update({
+    // DEPRECATED
+    hasChunk: async (email) => {
+        let chunk = await prisma.chunk.findFirst({
             where: {
-              id: newBulk.id,
+                user: {email},
+                createdAt: {
+                    gte: today,
+                },
             },
-            data: {
-              status: true,
+            select: {
+                id: true,
+                createdAt: true,
             },
-          });
-          return newBulk.id;
-        } else {
-          throw new Error("No New Bulk Found");
+        });
+        if (!chunk) return false;
+        return chunk["id"];
+    },
+
+    getAvailableChunkId: async (email) => {
+
+        let registeredChunk = await prisma.chunk.findFirst({
+            where: {
+                user: {email},
+                status: true,
+            },
+            select: {
+                id: true,
+                bulk: {
+                    select: {
+                        id: true,
+                        data: true,
+                        popIndex: true
+                    },
+                },
+            },
+        });
+        let bulkId = null;
+
+        if (registeredChunk) {
+
+            let i = 0;
+            while (typeof registeredChunk.bulk.data !== "object") {
+                registeredChunk.bulk.data = JSON.parse(registeredChunk.bulk.data);
+                if (i >= 4)
+                    break;
+            }
+
+            let bulk = registeredChunk.bulk.data;
+            let lastIndex = registeredChunk.bulk.popIndex;
+
+            if (!bulk.length || Number(lastIndex) >= bulk.length || Number(lastIndex) >= bulk.length - 1) {
+
+                let deActivatedBulk = await prisma.bulk.update({
+                    data: {
+                        status: false
+                    },
+                    where: {
+                        id: registeredChunk.bulk.id
+                    }
+                })
+
+                // ALTER TO NEW BULK IN LOOP
+                let newBulk = null;
+                let i = 0;
+                while (true) {
+                    let newInternalBulk = await prisma.bulk.findFirst({
+                        where: {
+                            id: {
+                                gt: bulk.id
+                            },
+                            popIndex: {
+                                equals: 0
+                            }
+                        },
+                        select: {
+                            id: true
+                        }
+                    })
+                    i++;
+                    if (newInternalBulk) {
+                        newBulk = newInternalBulk
+                        break;
+                    }
+                    if (i > 5) {
+                        break;
+                    }
+                }
+
+                if (!newBulk) {
+                    throw new Error("Could not find Bulk to replace");
+                }
+
+                // CONNECT ANOTHER BULK
+                let connectedChunk = await prisma.
+
+
+                bulkId = newBulk.id;
+            }
+
+            return registeredChunk.id;
         }
-      } else return currentBulk.id;
-    } else throw new Error("No Current Bulk");
-  },
 
-  // DEPRECATED
-  createChunk: async (email) => {
-    let hasChunk = await Chunk.hasChunk(email);
-    if (hasChunk) {
-      throw new Error("Chunk already exists");
-    }
 
-    let bulk = await prisma.bulk.findFirst({
-      where: {
-        status: true,
-      },
-      select: {
-        id: true,
-        createdAt: true,
-        data: true,
-        popIndex: true,
-        contactName: true,
-      },
-    });
-    if (!bulk) throw new Error("No Bulk found");
+        let createdChunk = await prisma.chunk.create({
+            data: {
+                user: {
+                    connect: {
+                        email,
+                    },
+                },
+                bulk: {
+                    connect: {
+                        id: bulkId,
+                    },
+                },
+            },
+        });
 
-    console.log("Active Bulk", bulk["id"]);
-    // CREATE CHUNK
-    let chunk = await prisma.chunk.create({
-      data: {
-        user: {
-          connect: { email },
-        },
-        bulk: {
-          connect: { id: bulk["id"] },
-        },
-      },
-      select: {
-        id: true,
-      },
-    });
-    return chunk["id"];
-  },
+        return createdChunk.id;
+    },
+
+    getAvailableBulkId: async () => {
+
+        let currentBulk = await prisma.bulk.findFirst({
+            where: {
+                status: true,
+            },
+            select: {
+                id: true,
+                data: true,
+                popIndex: true,
+            },
+        });
+
+        let i = 0;
+        while (typeof currentBulk.data !== "object") {
+            currentBulk.data = JSON.parse(currentBulk.data);
+            i++;
+            if (i >= 4) break;
+        }
+
+        if (currentBulk) {
+            const {popIndex, data} = currentBulk;
+            console.log("Bulk", currentBulk.id);
+
+            if (popIndex >= data.length - 1 || popIndex >= data.length) {
+                let updatedOldBulk = await prisma.bulk.update({
+                    where: {
+                        id: currentBulk.id,
+                    },
+                    data: {
+                        status: false,
+                    },
+                });
+
+                let newBulk = await prisma.bulk.findFirst({
+                    where: {
+                        status: false,
+                        id: {
+                            gte: currentBulk.id,
+                        },
+                    },
+                    select: {
+                        id: true,
+                        popIndex: true,
+                    },
+                });
+
+                if (newBulk && popIndex == 0) {
+                    let newBulk = await prisma.bulk.update({
+                        where: {
+                            id: newBulk.id,
+                        },
+                        data: {
+                            status: true,
+                        },
+                    });
+                    return newBulk.id;
+                } else {
+                    throw new Error("No New Bulk Found");
+                }
+            } else return currentBulk.id;
+        } else {
+
+            throw new Error("No Current Bulk");
+        }
+    },
+
+    // DEPRECATED
+    createChunk: async (email) => {
+        let hasChunk = await Chunk.hasChunk(email);
+        if (hasChunk) {
+            throw new Error("Chunk already exists");
+        }
+
+        let bulk = await prisma.bulk.findFirst({
+            where: {
+                status: true,
+            },
+            select: {
+                id: true,
+                createdAt: true,
+                data: true,
+                popIndex: true,
+                contactName: true,
+            },
+        });
+        if (!bulk) throw new Error("No Bulk found");
+
+        console.log("Active Bulk", bulk["id"]);
+        // CREATE CHUNK
+        let chunk = await prisma.chunk.create({
+            data: {
+                user: {
+                    connect: {email},
+                },
+                bulk: {
+                    connect: {id: bulk["id"]},
+                },
+            },
+            select: {
+                id: true,
+            },
+        });
+        return chunk["id"];
+    },
 };
 
 module.exports = Chunk;
--- user/index.js
+++ user/index.js
@@ -5,324 +5,321 @@ const bcryptjs = require("bcryptjs");
 const config = require("config");
 const Chunk = require("../chunk");
 
-const { PrismaClient } = require("@prisma/client");
+const {PrismaClient} = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 User.all("/", (req, res) => {
-  res.send("Not Allowed");
+    res.send("Not Allowed");
 });
 
 User.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(404).send("No such user found");
-    return;
-  }
-
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-      select: {
-        email: true,
-        password: true,
-        isAdmin: true,
-      },
-    });
-
-    if (!user) {
-      res.status(404).send("No such user found");
-      return;
+    const {email, password} = req.body;
+    if (!email || !password) {
+        res.status(404).send("No such user found");
+        return;
     }
 
-    let passwordCorrect = bcryptjs.compareSync(password, user["password"]);
-    if (!passwordCorrect) {
-      res.status(404).send("Password Incorrect");
-      return;
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+            select: {
+                email: true,
+                password: true,
+                isAdmin: true,
+            },
+        });
+
+        if (!user) {
+            res.status(404).send("No such user found");
+            return;
+        }
+
+        let passwordCorrect = bcryptjs.compareSync(password, user["password"]);
+        if (!passwordCorrect) {
+            res.status(404).send("Password Incorrect");
+            return;
+        }
+
+        delete user["password"];
+
+        if (!user.isAdmin) {
+            // GET AVAILABLE
+            // let chunk = await Chunk.hasChunk(email);
+            let chunk = await Chunk.getAvailableChunkId(email);
+            // console.log("Chunk-Id", chunk);
+            user["chunk"] = chunk;
+        }
+
+        let token = jwt.sign(user, config["secret"], {expiresIn: "1day"});
+        res.set("token", token).send(token);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Could not process.Contact Admin.");
     }
-
-    delete user["password"];
-
-    if (!user.isAdmin) {
-      // GET AVAILBLE
-      // let chunk = await Chunk.hasChunk(email);
-      let chunk = await Chunk.getAvailableChunkId(email);
-      // console.log("Chunk-Id", chunk);
-      user["chunk"] = chunk;
-    }
-
-    let token = jwt.sign(user, config["secret"], { expiresIn: "1day" });
-    res.set("token", token).send(token);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Could not process.Contact Admin.");
-  }
 });
 
 User.post("/call", async (req, res) => {
-  try {
-    const { chunk: chunkId, email } = res.locals.user;
+    try {
+        const {chunk: chunkId, email} = res.locals.user;
 
-    if (!chunkId) {
-      return res.status(404).send("Not Allowed to Call");
+        if (!chunkId) {
+            return res.status(404).send("Not Allowed to Call");
+        }
+
+        //NOT YET RESPONDED TO
+        let notYetRespondedTo = await prisma.call.findFirst({
+            where: {
+                respondedTo: false,
+                user: {
+                    email,
+                },
+            },
+            select: {
+                id: true,
+                contact: true,
+            },
+        });
+
+        if (notYetRespondedTo) {
+            res.json(notYetRespondedTo);
+            return;
+        }
+
+        // TODO GET NUMBER FROM BULK
+        let chunk = await prisma.chunk.findUnique({
+            where: {
+                id: chunkId,
+            },
+            select: {
+                id: true,
+                popCount: true,
+                bulk: {
+                    select: {
+                        id: true,
+                        data: true,
+                        popIndex: true,
+                        contactName: true,
+                    },
+                },
+            },
+        });
+
+        if (!chunk) {
+            res.status(404).send("No Chunk Created Re::Login");
+            return;
+        }
+
+        console.log("Chunk Id", chunk.id);
+        console.log("Bulk Id", chunk.bulk.id);
+        console.log("Last Pop Index", chunk.bulk.popIndex);
+        // res.json(JSON.parse(JSON.parse(chunk.bulk.data)));
+
+        let bulk = chunk.bulk.data;
+        let i = 0;
+        while (typeof bulk === "string") {
+            bulk = JSON.parse(bulk);
+            i++;
+            if (i >= 4) {
+                res.status(500).send("Data format error");
+                return;
+            }
+        }
+
+        // GET CONTACT
+        let {popIndex: lastIndex, contactName} = chunk.bulk;
+
+        if (!bulk.length || Number(lastIndex) >= bulk.length || Number(lastIndex) >= bulk.length - 1) {
+            res.status(404).send("Chunk out of data.Re::Login");
+            return;
+        }
+
+        let contact;
+        let others;
+        let otherUsers = null;
+
+        // TODO CHECK IF SUBSCRIBED IN WHILE LOOP
+        while (true) {
+            others = bulk[lastIndex];
+            contact = String(others[contactName]);
+
+            let checkContactIfCalled = await prisma.call.findUnique({
+                where: {
+                    contact: String(contact),
+                },
+                select: {
+                    id: true,
+                    contact: true,
+                    respondedTo: true,
+                    otherUsers: true,
+                },
+            });
+            if (!checkContactIfCalled) {
+                console.log("Never called");
+                break;
+            } else if (!checkContactIfCalled.respondedTo) {
+                console.log("Never responded to");
+                otherUsers = checkContactIfCalled.otherUsers || "";
+                break;
+            } else {
+                console.log("Contact Once Called And Responded");
+                lastIndex++;
+            }
+        }
+
+        console.log("Contact to be used", contact);
+
+        let updatedChunk = await prisma.chunk.update({
+            where: {id: chunk.id},
+            data: {popCount: chunk.popCount + 1},
+        });
+
+        let updatedBulk = await prisma.bulk.update({
+            where: {id: chunk.bulk.id},
+            data: {popIndex: lastIndex + 1},
+        });
+
+        const call = await prisma.call.upsert({
+            where: {
+                contact: String(contact),
+            },
+            create: {
+                user: {
+                    connect: {
+                        email,
+                    },
+                },
+                contact,
+            },
+            update: {
+                user: {
+                    connect: {
+                        email,
+                    },
+                },
+                contact,
+                otherUsers: otherUsers + email + ",",
+            },
+            select: {
+                id: true,
+                contact: true,
+            },
+        });
+
+        if (otherUsers) {
+            call.otherUsers = otherUsers;
+        }
+        if (others) {
+            call.others = others;
+        }
+
+        res.status(201).json(call);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Could not process");
     }
-
-    //NOT YET RESPONDED TO
-    let notYetRespondedTo = await prisma.call.findFirst({
-      where: {
-        respondedTo: false,
-        user: {
-          email,
-        },
-      },
-      select: {
-        id: true,
-        contact: true,
-      },
-    });
-
-    if (notYetRespondedTo) {
-      res.json(notYetRespondedTo);
-      return;
-    }
-
-    // TODO GET NUMBER FROM BULK
-    let chunk = await prisma.chunk.findUnique({
-      where: {
-        id: chunkId,
-      },
-      select: {
-        id: true,
-        popCount: true,
-        bulk: {
-          select: {
-            id: true,
-            data: true,
-            popIndex: true,
-            contactName: true,
-          },
-        },
-      },
-    });
-
-    if (!chunk) {
-      res.status(404).send("No Chunk Created Re::Login");
-      return;
-    }
-
-    console.log("Chunk Id", chunk.id);
-    console.log("Bulk Id", chunk.bulk.id);
-    console.log("Last Pop Index", chunk.bulk.popIndex);
-    // res.json(JSON.parse(JSON.parse(chunk.bulk.data)));
-
-    let bulk = chunk.bulk.data;
-    let i = 0;
-    while (typeof bulk === "string") {
-      bulk = JSON.parse(bulk);
-      i++;
-      if (i >= 4) {
-        res.status(500).send("Data format error");
-        return;
-      }
-    }
-
-    // GET CONTACT
-    let { popIndex: lastIndex, contactName } = chunk.bulk;
-
-    if (
-      !bulk.length ||
-      Number(lastIndex) >= bulk.length ||
-      Number(lastIndex) >= bulk.length - 1
-    ) {
-      res.status(404).send("Chunk out of data.Re::Login");
-    }
-
-    let contact;
-    let others;
-    let otherUsers = null;
-
-    // TODO CHECK IF SUBSCRIBED IN WHILE LOOP
-    while (true) {
-      others = bulk[lastIndex];
-      contact = String(others[contactName]);
-
-      let checkContactIfCalled = await prisma.call.findUnique({
-        where: {
-          contact: String(contact),
-        },
-        select: {
-          id: true,
-          contact: true,
-          respondedTo: true,
-          otherUsers: true,
-        },
-      });
-      if (!checkContactIfCalled) {
-        console.log("Never called");
-        break;
-      } else if (!checkContactIfCalled.respondedTo) {
-        console.log("Never responded to");
-        otherUsers = checkContactIfCalled.otherUsers || "";
-        break;
-      } else {
-        console.log("Contact Once Called And Responded");
-        lastIndex++;
-      }
-    }
-
-    console.log("Contact to be used", contact);
-
-    let updatedChunk = await prisma.chunk.update({
-      where: { id: chunk.id },
-      data: { popCount: chunk.popCount + 1 },
-    });
-
-    let updatedBulk = await prisma.bulk.update({
-      where: { id: chunk.bulk.id },
-      data: { popIndex: lastIndex + 1 },
-    });
-
-    const call = await prisma.call.upsert({
-      where: {
-        contact: String(contact),
-      },
-      create: {
-        user: {
-          connect: {
-            email,
-          },
-        },
-        contact,
-      },
-      update: {
-        user: {
-          connect: {
-            email,
-          },
-        },
-        contact,
-        otherUsers: otherUsers + email + ",",
-      },
-      select: {
-        id: true,
-        contact: true,
-      },
-    });
-
-    if (otherUsers) {
-      call.otherUsers = otherUsers;
-    }
-    if (others) {
-      call.others = others;
-    }
-
-    res.status(201).json(call);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Could not process");
-  }
 });
 
 User.post("/response", async (req, res) => {
-  try {
-    const {
-      id,
-      firstResponse,
-      gender,
-      lastResponse,
-      other,
-      likely,
-      resolution,
-    } = req.body;
+    try {
+        const {
+            id,
+            firstResponse,
+            gender,
+            lastResponse,
+            other,
+            likely,
+            resolution,
+        } = req.body;
 
-    if (!id || !firstResponse || firstResponse == "") {
-      res.status(404).send("No response data");
-      return;
-    }
+        if (!id || !firstResponse || firstResponse === "") {
+            res.status(404).send("No response data");
+            return;
+        }
 
-    const { email } = res.locals;
+        const {email} = res.locals;
 
-    let call = await prisma.call.findFirst({
-      where: {
-        id: Number(id),
-        user: {
-          email,
-        },
-      },
-      select: {
-        id: true,
-        contact: true,
-      },
-    });
-
-    if (!call) {
-      res.status(404).send("No such call for you");
-      return;
-    }
-
-    // IF NOT RECEIVED ::
-    if (firstResponse.toUpperCase() !== "RECEIVED") {
-      let response = await prisma.response.create({
-        data: {
-          firstResponse: firstResponse.toUpperCase(),
-          gender: "NONE",
-          lastResponse: "NO_RESPONSE",
-          call: {
-            connect: {
-              id: Number(id),
+        let call = await prisma.call.findFirst({
+            where: {
+                id: Number(id),
+                user: {
+                    email,
+                },
             },
-          },
-        },
-      });
-    } else {
-      if (!gender || !lastResponse || lastResponse == "") {
-        res.status(404).send("No response data");
-        return;
-      }
-
-      let response = await prisma.response.create({
-        data: {
-          firstResponse: firstResponse.toUpperCase(),
-          gender: gender.toUpperCase(),
-          lastResponse: lastResponse.toUpperCase(),
-          likely: Number(likely),
-          other,
-          resolution,
-          call: {
-            connect: {
-              id: Number(id),
+            select: {
+                id: true,
+                contact: true,
             },
-          },
-        },
-      });
+        });
+
+        if (!call) {
+            res.status(404).send("No such call for you");
+            return;
+        }
+
+        // IF NOT RECEIVED ::
+        if (firstResponse.toUpperCase() !== "RECEIVED") {
+            let response = await prisma.response.create({
+                data: {
+                    firstResponse: firstResponse.toUpperCase(),
+                    gender: "NONE",
+                    lastResponse: "NO_RESPONSE",
+                    call: {
+                        connect: {
+                            id: Number(id),
+                        },
+                    },
+                },
+            });
+        } else {
+            if (!gender || !lastResponse || lastResponse == "") {
+                res.status(404).send("No response data");
+                return;
+            }
+
+            let response = await prisma.response.create({
+                data: {
+                    firstResponse: firstResponse.toUpperCase(),
+                    gender: gender.toUpperCase(),
+                    lastResponse: lastResponse.toUpperCase(),
+                    likely: Number(likely),
+                    other,
+                    resolution,
+                    call: {
+                        connect: {
+                            id: Number(id),
+                        },
+                    },
+                },
+            });
+        }
+
+        let updatedCall = await prisma.call.update({
+            where: {
+                id: call.id,
+            },
+            data: {
+                respondedTo: true,
+            },
+        });
+
+        res.send("Call Ended");
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Could end call");
     }
-
-    let updatedCall = await prisma.call.update({
-      where: {
-        id: call.id,
-      },
-      data: {
-        respondedTo: true,
-      },
-    });
-
-    res.send("Call Ended");
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Could end call");
-  }
 });
 
 User.post("/options", async (req, res) => {
-  try {
-    const result = await prisma.$queryRaw("DESC Response;");
-    res.json(result);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Could not process");
-  }
+    try {
+        const result = await prisma.$queryRaw("DESC Response;");
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Could not process");
+    }
 });
 
 module.exports = User;

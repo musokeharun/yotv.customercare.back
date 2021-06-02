41339e63a90b44e6057290bb6964e3b554c66821
Haruna H <musokeharun@gmail.com>
2021-06-02 08:29:49 +0300

    Add Execptio Handler

--- server.js
+++ server.js
@@ -45,4 +45,18 @@ app.use("/admin", require("./admin"));
 // nodemon will watch any changes to server without restart
 const PORT = process.env.PORT || 5000;
 
+process.on("uncaughtException", (error, origin) => {
+  console.log("----- Uncaught exception -----");
+  console.log(error);
+  console.log("----- Exception origin -----");
+  console.log(origin);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("----- Unhandled Rejection at -----");
+  console.log(promise);
+  console.log("----- Reason -----");
+  console.log(reason);
+});
+
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
